59f5a56c4d68b368386acd5d6ac4062fc3ca6dee
tkobescak <32757441+tkobescak@users.noreply.github.com>
2018-01-21 14:18:12 +0100

    Add files via upload

---
+++ crud/read.php
@@ -0,0 +1,81 @@
+<?php
+// Check existence of id parameter before processing further
+if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
+    // Include config file
+    require_once 'config.php';
+    
+    // Prepare a select statement
+    $sql = "SELECT * FROM employees WHERE id = ?";
+    
+    if($stmt = mysqli_prepare($link, $sql)){
+        // Bind variables to the prepared statement as parameters
+        mysqli_stmt_bind_param($stmt, "i", $param_id);
+        
+        // Set parameters
+        $param_id = trim($_GET["id"]);
+        
+        // Attempt to execute the prepared statement
+        if(mysqli_stmt_execute($stmt)){
+            $result = mysqli_stmt_get_result($stmt);
+    
+            if(mysqli_num_rows($result) == 1){
+                /* Fetch result row as an associative array. Since the result set contains only one row, we don't need to use while loop */
+                $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+                
+                // Retrieve individual field value
+                $name = $row["name"];
+                $address = $row["address"];
+                $salary = $row["salary"];
+            } else{
+                // URL doesn't contain valid id parameter. Redirect to error page
+                header("location: error.php");
+                exit();
+            }
+            
+        } else{
+            echo "Oops! Something went wrong. Please try again later.";
+        }
+    }
+     
+    // Close statement
+    mysqli_stmt_close($stmt);
+    
+    // Close connection
+    mysqli_close($link);
+} else{
+    // URL doesn't contain id parameter. Redirect to error page
+    header("location: error.php");
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <meta charset="UTF-8">
+    <title>View Record</title>
+</head>
+<body>
+    <h1>View Record</h1>
+    <table id="read">
+        <thead>
+            <tr>               
+                <th>Name</th>
+                <th>Address</th>
+                <th>Salary</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td><?php echo $row["name"]; ?></td>
+                <td><?php echo $row["address"]; ?></td>
+                <td><?php echo $row["salary"]; ?></td>
+            </tr>
+        
+        </tbody>                            
+    </table><br>
+    <center><form action="index.php">
+        <input type="submit" value="Back">
+    </form></center>
+</body>
+</html>
